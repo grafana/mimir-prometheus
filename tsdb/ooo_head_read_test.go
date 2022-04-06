6660f617c4c2e51268f7b95cec485c52a33ed110
Dieter Plaetinck <dieter@grafana.com>
2022-04-06 18:46:34 +0300

    bugfix: we must auto-compute the last marker. there's no other way.
    precisely because the order is unpredictable

--- tsdb/ooo_head_read_test.go
+++ tsdb/ooo_head_read_test.go
@@ -20,23 +20,6 @@ type chunkInterval struct {
 	maxt int64
 }
 
-// GetChunkInterval returs the chunkInterval with matching ID, as well as what its chunkRef would be
-func GetChunkInterval(in []chunkInterval, ID int) (chunkInterval, chunks.ChunkRef) {
-	for ref, c := range in {
-		if c.ID == ID {
-			return c, chunks.ChunkRef(chunks.NewHeadChunkRef(1, chunks.HeadChunkID(ref)))
-		}
-	}
-	return chunkInterval{}, 0
-}
-
-type chunkOut struct {
-	ID     int // to match on the input chunk we're based on
-	lastID int // to match on the last matching chunk that dictates the markers
-	mint   int64
-	maxt   int64
-}
-
 // permutateChunkIntervals returns all possible orders of the given chunkIntervals
 func permutateChunkIntervals(in []chunkInterval, out [][]chunkInterval, left, right int) [][]chunkInterval {
 	if left == right {
@@ -64,7 +47,7 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 		queryMaxT           int64
 		inputChunkIntervals []chunkInterval
 		expSeriesError      bool
-		expChunks           []chunkOut
+		expChunks           []chunkInterval
 	}{
 		{
 			name:           "Test1: Empty result and no error when head is empty",
@@ -113,8 +96,8 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 			// Query Interval                [-----------------------------------------------------------]
 			// Chunk 0:                                 [---------------------------------------]
 			// Expected Output  Empty
-			expChunks: []chunkOut{
-				{0, 0, 150, 350},
+			expChunks: []chunkInterval{
+				{0, 150, 350},
 			},
 		},
 		{
@@ -129,8 +112,8 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 			// Query Interval:                          [---------------------------------------]
 			// Chunk 0:                       [-----------------------------------------------------------]
 			// Expected Output  Empty
-			expChunks: []chunkOut{
-				{0, 0, 100, 400},
+			expChunks: []chunkInterval{
+				{0, 100, 400},
 			},
 		},
 		{
@@ -152,9 +135,9 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 			// Chunk 3:                                                                                                                  [-------------------]
 			// Expected Output  [0x1000000, 0x1000001] with OOOLastReferences pointing to 0x1000003
 			// Output Graphically              [-----------------------------]                                                 [-----------------------------]
-			expChunks: []chunkOut{
-				{0, 3, 100, 250},
-				{1, 3, 500, 650},
+			expChunks: []chunkInterval{
+				{0, 100, 250},
+				{1, 500, 650},
 			},
 		},
 		{
@@ -176,8 +159,8 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 			// Chunk 3:                                                                                    [------------------]
 			// Expected Output  [0x1000000] with OOOLastReferences pointing to 0x1000003
 			// Output Graphically              [------------------------------------------------------------------------------]
-			expChunks: []chunkOut{
-				{0, 3, 100, 500},
+			expChunks: []chunkInterval{
+				{0, 100, 500},
 			},
 		},
 		{
@@ -199,11 +182,11 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 			// Chunk 3:                                                                                    [------------------]
 			// Expected Output  [0x1000000, 0x1000001, 0x1000002, 0x1000003] with OOOLastReferences pointing to 0x1000003
 			// Output Graphically              [------------------][------------------][------------------][------------------]
-			expChunks: []chunkOut{
-				{0, 3, 100, 199},
-				{1, 3, 200, 299},
-				{2, 3, 300, 399},
-				{3, 3, 400, 499},
+			expChunks: []chunkInterval{
+				{0, 100, 199},
+				{1, 200, 299},
+				{2, 300, 399},
+				{3, 400, 499},
 			},
 		},
 		{
@@ -225,8 +208,8 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 			// Chunk 3:                                                                                             [------------------]
 			// Expected Output  [0x1000000, 0x1000001, 0x1000002] with OOOLastReferences pointing to 0x1000002
 			// Output Graphically              [-----------------------------------------------]
-			expChunks: []chunkOut{
-				{0, 2, 100, 350},
+			expChunks: []chunkInterval{
+				{0, 100, 350},
 			},
 		},
 		{
@@ -250,9 +233,9 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 			// Chunk 4:                                                                                                                             [---------------------------------------]
 			// Expected Output  [0x1000000, 0x1000004] With OOOLastReferences pointing to 0v1000004
 			// Output Graphically              [---------------------------------------]                                                            [------------------------------------------------]
-			expChunks: []chunkOut{
-				{0, 4, 100, 300},
-				{4, 4, 600, 850},
+			expChunks: []chunkInterval{
+				{0, 100, 300},
+				{4, 600, 850},
 			},
 		},
 	}
@@ -281,23 +264,38 @@ func TestOOOHeadIndexReader_Series(t *testing.T) {
 
 					s1, _, _ := h.getOrCreate(s1ID, s1Lset)
 
-					// define our expected chunks, one for each defined expected chunk interval
-					// find the marker properties for last chunk, as well as the chunk Ref,
-					// by matching the chunks on their ID fields
+					var markerChunk chunkInterval
+					var markerPos int
+
+					// the marker should be set based on whichever is the last chunk/interval that overlaps with the query range
+					for i, interv := range intervals {
+						if overlapsClosedInterval(interv.mint, interv.maxt, tc.queryMinT, tc.queryMaxT) {
+							markerChunk = interv
+							markerPos = i
+						}
+					}
+					markerRef := chunks.ChunkRef(chunks.NewHeadChunkRef(1, chunks.HeadChunkID(uint64(markerPos))))
+
+					// define our expected chunks, by looking at the expected ChunkIntervals and setting...
 					var expChunks []chunks.Meta
 					for _, e := range tc.expChunks {
-						lastChunk, lastRef := GetChunkInterval(intervals, e.lastID)
 						meta := chunks.Meta{
-							Chunk:          chunkenc.Chunk(nil),
-							MinTime:        e.mint,
-							MaxTime:        e.maxt,
-							OOOLastMinTime: lastChunk.mint,
-							OOOLastMaxTime: lastChunk.maxt,
-							OOOLastRef:     lastRef,
+							Chunk:   chunkenc.Chunk(nil),
+							MinTime: e.mint,
+							MaxTime: e.maxt,
+							// 1) markers based on the last chunk we found above
+							OOOLastMinTime: markerChunk.mint,
+							OOOLastMaxTime: markerChunk.maxt,
+							OOOLastRef:     markerRef,
 						}
 
-						_, meta.Ref = GetChunkInterval(intervals, e.ID)
-
+						// 3) Ref to whatever Ref the chunk has, that we refer to by ID
+						for ref, c := range intervals {
+							if c.ID == e.ID {
+								meta.Ref = chunks.ChunkRef(chunks.NewHeadChunkRef(1, chunks.HeadChunkID(ref)))
+								break
+							}
+						}
 						expChunks = append(expChunks, meta)
 					}
 
