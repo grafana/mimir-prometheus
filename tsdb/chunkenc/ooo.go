5e2d6a2e236e27337751741c992343228e9bbda1
Jesus Vazquez <jesus.vazquez@grafana.com>
2022-04-19 22:27:40 +0200

    Export OOOChunk Samples T and V
    In order to use the fields from the ooo head to iterate the samples
    we're exporting them.
    
    This was the quick way to do it, we could implement an iterator to keep
    the chunk private. I just want to illustrate how it would work.

--- tsdb/chunkenc/ooo.go
+++ tsdb/chunkenc/ooo.go
@@ -4,49 +4,49 @@ import (
 	"sort"
 )
 
-type sample struct {
-	t int64
-	v float64
+type Sample struct {
+	T int64
+	V float64
 }
 
-// OOOChunk maintains samples in time-ascending order.
+// OOOChunk maintains Samples in time-ascending order.
 // Inserts for timestamps already seen, are dropped.
 // Samples are stored uncompressed to allow easy sorting.
 // Perhaps we can be more efficient later.
 type OOOChunk struct {
-	samples []sample
+	Samples []Sample
 }
 
 func NewOOOChunk(capacity int) *OOOChunk {
-	return &OOOChunk{samples: make([]sample, 0, capacity)}
+	return &OOOChunk{Samples: make([]Sample, 0, capacity)}
 }
 
-// Insert inserts the sample such that order is maintained.
+// Insert inserts the Sample such that order is maintained.
 // Returns false if insert was not possible due to the same timestamp already existing.
 func (o *OOOChunk) Insert(t int64, v float64) bool {
-	// find index of sample we should replace
-	i := sort.Search(len(o.samples), func(i int) bool { return o.samples[i].t >= t })
+	// find index of Sample we should replace
+	i := sort.Search(len(o.Samples), func(i int) bool { return o.Samples[i].T >= t })
 
-	if i >= len(o.samples) {
+	if i >= len(o.Samples) {
 		// none found. append it at the end
-		o.samples = append(o.samples, sample{t, v})
+		o.Samples = append(o.Samples, Sample{t, v})
 		return true
 	}
 
-	if o.samples[i].t == t {
+	if o.Samples[i].T == t {
 		return false
 	}
 
-	// expand length by 1 to make room. use a zero sample, we will overwrite it anyway
-	o.samples = append(o.samples, sample{})
-	copy(o.samples[i+1:], o.samples[i:])
-	o.samples[i] = sample{t, v}
+	// expand length by 1 to make room. use a zero Sample, we will overwrite it anyway
+	o.Samples = append(o.Samples, Sample{})
+	copy(o.Samples[i+1:], o.Samples[i:])
+	o.Samples[i] = Sample{t, v}
 
 	return true
 }
 
 func (o *OOOChunk) NumSamples() int {
-	return len(o.samples)
+	return len(o.Samples)
 }
 
 func (o *OOOChunk) ToXor() (*XORChunk, error) {
@@ -55,8 +55,8 @@ func (o *OOOChunk) ToXor() (*XORChunk, error) {
 	if err != nil {
 		return nil, err
 	}
-	for _, s := range o.samples {
-		app.Append(s.t, s.v)
+	for _, s := range o.Samples {
+		app.Append(s.T, s.V)
 	}
 	return x, nil
 }
@@ -67,7 +67,7 @@ func (o *OOOChunk) ToXor() (*XORChunk, error) {
 // func (c *OOOChunk) NumSamples() int {
 // func (c *OOOChunk) Compact() {
 
-// func (it *oooIterator) Seek(t int64) bool {
+// func (it *oooIterator) Seek(T int64) bool {
 // func (it *oooIterator) At() (int64, float64) {
 // func (it *oooIterator) Err() error {
 // func (it *oooIterator) Next() bool {
--- tsdb/chunkenc/ooo_test.go
+++ tsdb/chunkenc/ooo_test.go
@@ -12,10 +12,10 @@ const testMaxSize int = 32
 func valPre(pos int) int { return pos*2 + 2 } // s[0]=2, s[1]=4, s[2]=6, ..., s[31]=64 // predictable pre-existing values
 func valNew(pos int) int { return pos*2 + 1 } // s[0]=1, s[1]=3, s[2]=5, ..., s[31]=63 // new values will interject at chosen position because they sort before the pre-existing vals
 
-func samplify(v int) sample { return sample{int64(v), float64(v)} }
+func samplify(v int) Sample { return Sample{int64(v), float64(v)} }
 
-func makePre(n int) []sample {
-	s := make([]sample, n)
+func makePre(n int) []Sample {
+	s := make([]Sample, n)
 	for i := 0; i < n; i++ {
 		s[i] = samplify(valPre(i))
 	}
@@ -23,61 +23,61 @@ func makePre(n int) []sample {
 }
 
 // TesttoooInsert tests the following cases:
-// number of pre-existing samples anywhere from 0 to testMaxSize-1
-// insert new sample before first pre-existing samples, after the last, and anywhere in between
+// number of pre-existing Samples anywhere from 0 to testMaxSize-1
+// insert new Sample before first pre-existing Samples, after the last, and anywhere in between
 // with a chunk initial capacity of testMaxSize/8 and testMaxSize, which lets us test non-full and full chunks, and chunks that need to expand themselves.
-// Note: in all samples used, t always equals v in numeric value. when we talk about 'value' we just refer to a value that will be used for both sample.t and sample.v
+// Note: in all Samples used, T always equals V in numeric value. when we talk about 'value' we just refer to a value that will be used for both Sample.T and Sample.V
 func TestOOOInsert(t *testing.T) {
 	for numPre := 0; numPre <= testMaxSize; numPre++ {
 		// for example, if we have numPre 2, then:
-		// chunk.samples indexes filled        0   1
-		// chunk.samples with these values     2   4     // valPre
+		// chunk.Samples indexes filled        0   1
+		// chunk.Samples with these values     2   4     // valPre
 		// we want to test inserting at index  0   1   2 // insertPos=0..numPre
 		// we can do this by using values      1,  3   5 // valNew(insertPos)
 
 		for insertPos := 0; insertPos <= numPre; insertPos++ {
 			for capacity := range []int{testMaxSize / 8, testMaxSize} {
 				chunk := NewOOOChunk(capacity)
-				chunk.samples = makePre(numPre)
+				chunk.Samples = makePre(numPre)
 				newSample := samplify(valNew(insertPos))
-				chunk.Insert(newSample.t, newSample.v)
+				chunk.Insert(newSample.T, newSample.V)
 
-				var expSamples []sample
-				// our expected new samples slice, will be first the original samples...
+				var expSamples []Sample
+				// our expected new Samples slice, will be first the original Samples...
 				for i := 0; i < insertPos; i++ {
 					expSamples = append(expSamples, samplify(valPre(i)))
 				}
-				// ... then the new sample ...
+				// ... then the new Sample ...
 				expSamples = append(expSamples, newSample)
-				// ... followed by any original samples that were pushed back by the new one
+				// ... followed by any original Samples that were pushed back by the new one
 				for i := insertPos; i < numPre; i++ {
 					expSamples = append(expSamples, samplify(valPre(i)))
 				}
 
-				require.Equal(t, expSamples, chunk.samples, "numPre %d, insertPos %d", numPre, insertPos)
+				require.Equal(t, expSamples, chunk.Samples, "numPre %d, insertPos %d", numPre, insertPos)
 			}
 		}
 	}
 }
 
-// TesttoooInsertDuplicate tests the correct behavior when inserting a sample that is a duplicate of any
-// pre-existing samples, with between 1 and testMaxSize pre-existing samples and
+// TesttoooInsertDuplicate tests the correct behavior when inserting a Sample that is a duplicate of any
+// pre-existing Samples, with between 1 and testMaxSize pre-existing Samples and
 // with a chunk initial capacity of testMaxSize/8 and testMaxSize, which lets us test non-full and full chunks, and chunks that need to expand themselves.
 func TestOOOInsertDuplicate(t *testing.T) {
 	for numPre := 1; numPre <= testMaxSize; numPre++ {
 		for dupPos := 0; dupPos < numPre; dupPos++ {
 			for capacity := range []int{testMaxSize / 8, testMaxSize} {
 				chunk := NewOOOChunk(capacity)
-				chunk.samples = makePre(numPre)
+				chunk.Samples = makePre(numPre)
 
-				dupSample := chunk.samples[dupPos]
-				dupSample.v = 0.123 // unmistakeably different from any of the pre-existing values, so we can properly detect the correct value below
+				dupSample := chunk.Samples[dupPos]
+				dupSample.V = 0.123 // unmistakeably different from any of the pre-existing values, so we can properly detect the correct value below
 
-				ok := chunk.Insert(dupSample.t, dupSample.v)
+				ok := chunk.Insert(dupSample.T, dupSample.V)
 
 				expSamples := makePre(numPre) // we expect no change
 				require.False(t, ok)
-				require.Equal(t, expSamples, chunk.samples, "numPre %d, dupPos %d", numPre, dupPos)
+				require.Equal(t, expSamples, chunk.Samples, "numPre %d, dupPos %d", numPre, dupPos)
 			}
 		}
 	}
