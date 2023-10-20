8061c62ccfd4502f003826baa4483e84348f5c86
Fiona Liao <fiona.y.liao@gmail.com>
2023-10-20 17:40:25 +0100

    nolint:staticcheck

--- tsdb/head_wal.go
+++ tsdb/head_wal.go
@@ -253,7 +253,7 @@ Outer:
 				idx := uint64(mSeries.ref) % uint64(concurrency)
 				processors[idx].input <- walSubsetProcessorInputItem{walSeriesRef: walSeries.Ref, existingSeries: mSeries}
 			}
-			seriesPool.Put(v)
+			seriesPool.Put(v) //nolint:staticcheck
 		case []record.RefSample:
 			samples := v
 			minValidTime := h.minValidTime.Load()
@@ -289,7 +289,7 @@ Outer:
 				}
 				samples = samples[m:]
 			}
-			samplesPool.Put(v)
+			samplesPool.Put(v) //nolint:staticcheck
 		case []tombstones.Stone:
 			for _, s := range v {
 				for _, itv := range s.Intervals {
@@ -303,12 +303,12 @@ Outer:
 					h.tombstones.AddInterval(s.Ref, itv)
 				}
 			}
-			tstonesPool.Put(v)
+			tstonesPool.Put(v) //nolint:staticcheck
 		case []record.RefExemplar:
 			for _, e := range v {
 				exemplarsInput <- e
 			}
-			exemplarsPool.Put(v)
+			exemplarsPool.Put(v) //nolint:staticcheck
 		case []record.RefHistogramSample:
 			samples := v
 			minValidTime := h.minValidTime.Load()
@@ -344,7 +344,7 @@ Outer:
 				}
 				samples = samples[m:]
 			}
-			histogramsPool.Put(v)
+			histogramsPool.Put(v) //nolint:staticcheck
 		case []record.RefFloatHistogramSample:
 			samples := v
 			minValidTime := h.minValidTime.Load()
@@ -380,7 +380,7 @@ Outer:
 				}
 				samples = samples[m:]
 			}
-			floatHistogramsPool.Put(v)
+			floatHistogramsPool.Put(v) //nolint:staticcheck
 		case []record.RefMetadata:
 			for _, m := range v {
 				s := h.series.getByID(m.Ref)
@@ -394,7 +394,7 @@ Outer:
 					Help: m.Help,
 				}
 			}
-			metadataPool.Put(v)
+			metadataPool.Put(v) //nolint:staticcheck
 		default:
 			panic(fmt.Errorf("unexpected decoded type: %T", d))
 		}
@@ -861,7 +861,7 @@ func (h *Head) loadWBL(r *wlog.Reader, multiRef map[chunks.HeadSeriesRef]chunks.
 				}
 				samples = samples[m:]
 			}
-			histogramSamplesPool.Put(v)
+			histogramSamplesPool.Put(v) //nolint:staticcheck
 		case []record.RefFloatHistogramSample:
 			samples := v
 			// We split up the samples into chunks of 5000 samples or less.
@@ -893,7 +893,7 @@ func (h *Head) loadWBL(r *wlog.Reader, multiRef map[chunks.HeadSeriesRef]chunks.
 				}
 				samples = samples[m:]
 			}
-			floatHistogramSamplesPool.Put(v)
+			floatHistogramSamplesPool.Put(v) //nolint:staticcheck
 		default:
 			panic(fmt.Errorf("unexpected decodedCh type: %T", d))
 		}
