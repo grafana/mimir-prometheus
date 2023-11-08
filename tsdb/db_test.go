a15c1ecd48071de4f4e68f97ca5e1e8224d6ba21
Fiona Liao <fiona.y.liao@gmail.com>
2023-11-08 10:39:54 +0000

    Improve requireEqualSamples (#565)
    - Allow counter reset header to be compared
    - Add timestamp value check
    - Add timestamp to error message
    
    Signed-off-by: Fiona Liao <fiona.y.liao@gmail.com>

--- tsdb/db_test.go
+++ tsdb/db_test.go
@@ -4682,7 +4682,7 @@ func testOOOCompaction(t *testing.T, scenario sampleTypeScenario) {
 		q, err := db.Querier(math.MinInt64, math.MaxInt64)
 		require.NoError(t, err)
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	verifyDBSamples() // Before any compaction.
@@ -4908,7 +4908,7 @@ func testOOOCompactionWithNormalCompaction(t *testing.T, scenario sampleTypeScen
 		require.NoError(t, err)
 
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	// Checking for expected data in the blocks.
@@ -5019,7 +5019,7 @@ func testOOOCompactionWithDisabledWriteLog(t *testing.T, scenario sampleTypeScen
 		require.NoError(t, err)
 
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	// Checking for expected data in the blocks.
@@ -5122,7 +5122,7 @@ func testOOOQueryAfterRestartWithSnapshotAndRemovedWBL(t *testing.T, scenario sa
 		require.NoError(t, err)
 
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	// Checking for expected ooo data from mmap chunks.
@@ -5658,7 +5658,7 @@ func testOOOAppendAndQuery(t *testing.T, scenario sampleTypeScenario) {
 				expSamples[k] = append(expSamples[k], s)
 			}
 		}
-		requireEqualSamples(t, expSamples, seriesSet)
+		requireEqualSamples(t, expSamples, seriesSet, true)
 		require.Equal(t, float64(totalSamples-2), prom_testutil.ToFloat64(db.head.metrics.outOfOrderSamplesAppended), "number of ooo appended samples mismatch")
 	}
 
@@ -5776,7 +5776,7 @@ func testOOODisabled(t *testing.T, scenario sampleTypeScenario) {
 	require.NoError(t, err)
 
 	seriesSet := query(t, querier, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar."))
-	requireEqualSamples(t, expSamples, seriesSet)
+	requireEqualSamples(t, expSamples, seriesSet, true)
 	require.Equal(t, float64(0), prom_testutil.ToFloat64(db.head.metrics.outOfOrderSamplesAppended), "number of ooo appended samples mismatch")
 	require.Equal(t, float64(failedSamples),
 		prom_testutil.ToFloat64(db.head.metrics.outOfOrderSamples.WithLabelValues(scenario.sampleType))+prom_testutil.ToFloat64(db.head.metrics.outOfBoundSamples.WithLabelValues(scenario.sampleType)),
@@ -5844,7 +5844,7 @@ func testWBLAndMmapReplay(t *testing.T, scenario sampleTypeScenario) {
 			})
 			exp[k] = v
 		}
-		requireEqualSamples(t, exp, seriesSet)
+		requireEqualSamples(t, exp, seriesSet, true)
 	}
 
 	// In-order samples.
@@ -6125,7 +6125,7 @@ func testOOOCompactionFailure(t *testing.T, scenario sampleTypeScenario) {
 		q, err := NewBlockQuerier(block, math.MinInt64, math.MaxInt64)
 		require.NoError(t, err)
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	// Checking for expected data in the blocks.
@@ -6362,7 +6362,7 @@ func testOOOMmapCorruption(t *testing.T, scenario sampleTypeScenario) {
 		require.NoError(t, err)
 
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	verifySamples(allSamples)
@@ -6491,7 +6491,7 @@ func testOutOfOrderRuntimeConfig(t *testing.T, scenario sampleTypeScenario) {
 		require.NoError(t, err)
 
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	doOOOCompaction := func(t *testing.T, db *DB) {
@@ -6702,7 +6702,7 @@ func testNoGapAfterRestartWithOOO(t *testing.T, scenario sampleTypeScenario) {
 		require.NoError(t, err)
 
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	cases := []struct {
@@ -6832,7 +6832,7 @@ func testWblReplayAfterOOODisableAndRestart(t *testing.T, scenario sampleTypeSce
 		require.NoError(t, err)
 
 		actRes := query(t, q, labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar.*"))
-		requireEqualSamples(t, expRes, actRes)
+		requireEqualSamples(t, expRes, actRes, true)
 	}
 
 	verifySamples(allSamples)
