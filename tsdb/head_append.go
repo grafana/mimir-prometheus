3776156a8e3d2a1bf3a76ec790450892320396bb
Bryan Boreham <bjboreham@gmail.com>
2024-03-05 10:50:43 +0000

    tsdb: zero out Labels pointers in pool
    So that memory is not kept alive for garbage-collection.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- tsdb/head_append.go
+++ tsdb/head_append.go
@@ -243,6 +243,9 @@ func (h *Head) putExemplarBuffer(b []exemplarWithSeriesRef) {
 	if b == nil {
 		return
 	}
+	for i := range b { // Zero out to avoid retaining label data
+		b[i].exemplar.Labels = labels.EmptyLabels()
+	}
 
 	h.exemplarsPool.Put(b[:0])
 }
@@ -292,6 +295,9 @@ func (h *Head) getSeriesBuffer() []*memSeries {
 }
 
 func (h *Head) putSeriesBuffer(b []*memSeries) {
+	for i := range b { // Zero out to avoid retaining data
+		b[i] = nil
+	}
 	h.seriesPool.Put(b[:0])
 }
 
