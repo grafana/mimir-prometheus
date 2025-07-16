2b1040d260950236cb8d7363aff485332801c833
Arve Knudsen <arve.knudsen@gmail.com>
2025-07-16 11:50:27 +0200

    Don't change prometheus/common API
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -141,6 +141,10 @@ var (
 )
 
 func init() {
+	// This can be removed when the legacy global mode is fully deprecated.
+	//nolint:staticcheck
+	model.NameValidationScheme = model.UTF8Validation
+
 	prometheus.MustRegister(versioncollector.NewCollector(strings.ReplaceAll(appName, "-", "_")))
 
 	var err error
