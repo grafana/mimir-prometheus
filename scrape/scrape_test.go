prometheus/prometheus

Parameterize metric/label name validation scheme
Parameterized metric/label name validation scheme

Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

scrape/scrape_test.go
@@ -334,9 +334,10 @@ func TestDroppedTargetsList(t *testing.T) {
 			MetricNameEscapingScheme:   model.AllowUTF8,
 			RelabelConfigs: []*relabel.Config{
 				{
-					Action:       relabel.Drop,
-					Regex:        relabel.MustNewRegexp("dropMe"),
-					SourceLabels: model.LabelNames{"job"},
+					Action:                     relabel.Drop,
+					Regex:                      relabel.MustNewRegexp("dropMe"),
+					SourceLabels:               model.LabelNames{"job"},
+					MetricNameValidationScheme: model.UTF8Validation,
 				},
 			},
 		}
@@ -1367,10 +1368,11 @@ func TestScrapeLoopFailWithInvalidLabelsAfterRelabel(t *testing.T) {
 		labels: labels.FromStrings("pod_label_invalid_012\xff", "test"),
 	}
 	relabelConfig := []*relabel.Config{{
-		Action:      relabel.LabelMap,
-		Regex:       relabel.MustNewRegexp("pod_label_invalid_(.+)"),
-		Separator:   ";",
-		Replacement: "$1",
+		Action:                     relabel.LabelMap,
+		Regex:                      relabel.MustNewRegexp("pod_label_invalid_(.+)"),
+		Separator:                  ";",
+		Replacement:                "$1",
+		MetricNameValidationScheme: model.UTF8Validation,
 	}}
 	sl := newBasicScrapeLoop(t, ctx, &testScraper{}, s.Appender, 0)
 	sl.sampleMutator = func(l labels.Labels) labels.Labels {
@@ -4182,18 +4184,20 @@ func TestTargetScrapeIntervalAndTimeoutRelabel(t *testing.T) {
 		MetricNameEscapingScheme:   model.AllowUTF8,
 		RelabelConfigs: []*relabel.Config{
 			{
-				SourceLabels: model.LabelNames{model.ScrapeIntervalLabel},
-				Regex:        relabel.MustNewRegexp("2s"),
-				Replacement:  "3s",
-				TargetLabel:  model.ScrapeIntervalLabel,
-				Action:       relabel.Replace,
+				SourceLabels:               model.LabelNames{model.ScrapeIntervalLabel},
+				Regex:                      relabel.MustNewRegexp("2s"),
+				Replacement:                "3s",
+				TargetLabel:                model.ScrapeIntervalLabel,
+				Action:                     relabel.Replace,
+				MetricNameValidationScheme: model.UTF8Validation,
 			},
 			{
-				SourceLabels: model.LabelNames{model.ScrapeTimeoutLabel},
-				Regex:        relabel.MustNewRegexp("500ms"),
-				Replacement:  "750ms",
-				TargetLabel:  model.ScrapeTimeoutLabel,
-				Action:       relabel.Replace,
+				SourceLabels:               model.LabelNames{model.ScrapeTimeoutLabel},
+				Regex:                      relabel.MustNewRegexp("500ms"),
+				Replacement:                "750ms",
+				TargetLabel:                model.ScrapeTimeoutLabel,
+				Action:                     relabel.Replace,
+				MetricNameValidationScheme: model.UTF8Validation,
 			},
 		},
 	}
@@ -4220,20 +4224,22 @@ func TestLeQuantileReLabel(t *testing.T) {
 		JobName: "test",
 		MetricRelabelConfigs: []*relabel.Config{
 			{
-				SourceLabels: model.LabelNames{"le", "__name__"},
-				Regex:        relabel.MustNewRegexp("(\\d+)\\.0+;.*_bucket"),
-				Replacement:  relabel.DefaultRelabelConfig.Replacement,
-				Separator:    relabel.DefaultRelabelConfig.Separator,
-				TargetLabel:  "le",
-				Action:       relabel.Replace,
+				SourceLabels:               model.LabelNames{"le", "__name__"},
+				Regex:                      relabel.MustNewRegexp("(\\d+)\\.0+;.*_bucket"),
+				Replacement:                relabel.DefaultRelabelConfig.Replacement,
+				Separator:                  relabel.DefaultRelabelConfig.Separator,
+				TargetLabel:                "le",
+				Action:                     relabel.Replace,
+				MetricNameValidationScheme: model.UTF8Validation,
 			},
 			{
-				SourceLabels: model.LabelNames{"quantile"},
-				Regex:        relabel.MustNewRegexp("(\\d+)\\.0+"),
-				Replacement:  relabel.DefaultRelabelConfig.Replacement,
-				Separator:    relabel.DefaultRelabelConfig.Separator,
-				TargetLabel:  "quantile",
-				Action:       relabel.Replace,
+				SourceLabels:               model.LabelNames{"quantile"},
+				Regex:                      relabel.MustNewRegexp("(\\d+)\\.0+"),
+				Replacement:                relabel.DefaultRelabelConfig.Replacement,
+				Separator:                  relabel.DefaultRelabelConfig.Separator,
+				TargetLabel:                "quantile",
+				Action:                     relabel.Replace,
+				MetricNameValidationScheme: model.UTF8Validation,
 			},
 		},
 		SampleLimit:                100,
@@ -4862,18 +4868,20 @@ func TestTypeUnitReLabel(t *testing.T) {
 		JobName: "test",
 		MetricRelabelConfigs: []*relabel.Config{
 			{
-				SourceLabels: model.LabelNames{"__name__"},
-				Regex:        relabel.MustNewRegexp(".*_total$"),
-				Replacement:  "counter",
-				TargetLabel:  "__type__",
-				Action:       relabel.Replace,
+				SourceLabels:               model.LabelNames{"__name__"},
+				Regex:                      relabel.MustNewRegexp(".*_total$"),
+				Replacement:                "counter",
+				TargetLabel:                "__type__",
+				Action:                     relabel.Replace,
+				MetricNameValidationScheme: model.UTF8Validation,
 			},
 			{
-				SourceLabels: model.LabelNames{"__name__"},
-				Regex:        relabel.MustNewRegexp(".*_bytes$"),
-				Replacement:  "bytes",
-				TargetLabel:  "__unit__",
-				Action:       relabel.Replace,
+				SourceLabels:               model.LabelNames{"__name__"},
+				Regex:                      relabel.MustNewRegexp(".*_bytes$"),
+				Replacement:                "bytes",
+				TargetLabel:                "__unit__",
+				Action:                     relabel.Replace,
+				MetricNameValidationScheme: model.UTF8Validation,
 			},
 		},
 		SampleLimit:                100,
@@ -5482,25 +5490,28 @@ func TestTargetScrapeConfigWithLabels(t *testing.T) {
 				Params:                     url.Values{"param": []string{secondParam}},
 				RelabelConfigs: []*relabel.Config{
 					{
-						Action:       relabel.DefaultRelabelConfig.Action,
-						Regex:        relabel.DefaultRelabelConfig.Regex,
-						SourceLabels: relabel.DefaultRelabelConfig.SourceLabels,
-						TargetLabel:  model.ScrapeTimeoutLabel,
-						Replacement:  expectedTimeoutLabel,
+						Action:                     relabel.DefaultRelabelConfig.Action,
+						Regex:                      relabel.DefaultRelabelConfig.Regex,
+						SourceLabels:               relabel.DefaultRelabelConfig.SourceLabels,
+						TargetLabel:                model.ScrapeTimeoutLabel,
+						Replacement:                expectedTimeoutLabel,
+						MetricNameValidationScheme: model.UTF8Validation,
 					},
 					{
-						Action:       relabel.DefaultRelabelConfig.Action,
-						Regex:        relabel.DefaultRelabelConfig.Regex,
-						SourceLabels: relabel.DefaultRelabelConfig.SourceLabels,
-						TargetLabel:  paramLabel,
-						Replacement:  expectedParam,
+						Action:                     relabel.DefaultRelabelConfig.Action,
+						Regex:                      relabel.DefaultRelabelConfig.Regex,
+						SourceLabels:               relabel.DefaultRelabelConfig.SourceLabels,
+						TargetLabel:                paramLabel,
+						Replacement:                expectedParam,
+						MetricNameValidationScheme: model.UTF8Validation,
 					},
 					{
-						Action:       relabel.DefaultRelabelConfig.Action,
-						Regex:        relabel.DefaultRelabelConfig.Regex,
-						SourceLabels: relabel.DefaultRelabelConfig.SourceLabels,
-						TargetLabel:  model.MetricsPathLabel,
-						Replacement:  expectedPath,
+						Action:                     relabel.DefaultRelabelConfig.Action,
+						Regex:                      relabel.DefaultRelabelConfig.Regex,
+						SourceLabels:               relabel.DefaultRelabelConfig.SourceLabels,
+						TargetLabel:                model.MetricsPathLabel,
+						Replacement:                expectedPath,
+						MetricNameValidationScheme: model.UTF8Validation,
 					},
 				},
 			},

storage/remote/write_test.go
@@ -281,7 +281,8 @@ func TestWriteStorageApplyConfig_PartialUpdate(t *testing.T) {
 		QueueConfig:   config.DefaultQueueConfig,
 		WriteRelabelConfigs: []*relabel.Config{
 			{
-				Regex: relabel.MustNewRegexp(".+"),
+				Regex:                      relabel.MustNewRegexp(".+"),
+				MetricNameValidationScheme: model.UTF8Validation,
 			},
 		},
 		ProtobufMessage: config.RemoteWriteProtoMsgV1,
@@ -328,7 +329,10 @@ func TestWriteStorageApplyConfig_PartialUpdate(t *testing.T) {
 
 	storeHashes()
 	// Update c0 and c2.
-	c0.WriteRelabelConfigs[0] = &relabel.Config{Regex: relabel.MustNewRegexp("foo")}
+	c0.WriteRelabelConfigs[0] = &relabel.Config{
+		Regex:                      relabel.MustNewRegexp("foo"),
+		MetricNameValidationScheme: model.UTF8Validation,
+	}
 	c2.RemoteTimeout = model.Duration(50 * time.Second)
 	conf = &config.Config{
 		GlobalConfig:       config.GlobalConfig{},

grafana/mimir-prometheus

scrape/scrape_test.go
@@ -334,9 +334,10 @@ func TestDroppedTargetsList(t *testing.T) {
 			MetricNameEscapingScheme:   model.AllowUTF8,
 			RelabelConfigs: []*relabel.Config{
 				{
-					Action:       relabel.Drop,
-					Regex:        relabel.MustNewRegexp("dropMe"),
-					SourceLabels: model.LabelNames{"job"},
+					Action:               relabel.Drop,
+					Regex:                relabel.MustNewRegexp("dropMe"),
+					SourceLabels:         model.LabelNames{"job"},
+					NameValidationScheme: model.UTF8Validation,
 				},
 			},
 		}
@@ -1367,10 +1368,11 @@ func TestScrapeLoopFailWithInvalidLabelsAfterRelabel(t *testing.T) {
 		labels: labels.FromStrings("pod_label_invalid_012\xff", "test"),
 	}
 	relabelConfig := []*relabel.Config{{
-		Action:      relabel.LabelMap,
-		Regex:       relabel.MustNewRegexp("pod_label_invalid_(.+)"),
-		Separator:   ";",
-		Replacement: "$1",
+		Action:               relabel.LabelMap,
+		Regex:                relabel.MustNewRegexp("pod_label_invalid_(.+)"),
+		Separator:            ";",
+		Replacement:          "$1",
+		NameValidationScheme: model.UTF8Validation,
 	}}
 	sl := newBasicScrapeLoop(t, ctx, &testScraper{}, s.Appender, 0)
 	sl.sampleMutator = func(l labels.Labels) labels.Labels {
@@ -4182,18 +4184,20 @@ func TestTargetScrapeIntervalAndTimeoutRelabel(t *testing.T) {
 		MetricNameEscapingScheme:   model.AllowUTF8,
 		RelabelConfigs: []*relabel.Config{
 			{
-				SourceLabels: model.LabelNames{model.ScrapeIntervalLabel},
-				Regex:        relabel.MustNewRegexp("2s"),
-				Replacement:  "3s",
-				TargetLabel:  model.ScrapeIntervalLabel,
-				Action:       relabel.Replace,
+				SourceLabels:         model.LabelNames{model.ScrapeIntervalLabel},
+				Regex:                relabel.MustNewRegexp("2s"),
+				Replacement:          "3s",
+				TargetLabel:          model.ScrapeIntervalLabel,
+				Action:               relabel.Replace,
+				NameValidationScheme: model.UTF8Validation,
 			},
 			{
-				SourceLabels: model.LabelNames{model.ScrapeTimeoutLabel},
-				Regex:        relabel.MustNewRegexp("500ms"),
-				Replacement:  "750ms",
-				TargetLabel:  model.ScrapeTimeoutLabel,
-				Action:       relabel.Replace,
+				SourceLabels:         model.LabelNames{model.ScrapeTimeoutLabel},
+				Regex:                relabel.MustNewRegexp("500ms"),
+				Replacement:          "750ms",
+				TargetLabel:          model.ScrapeTimeoutLabel,
+				Action:               relabel.Replace,
+				NameValidationScheme: model.UTF8Validation,
 			},
 		},
 	}
@@ -4220,20 +4224,22 @@ func TestLeQuantileReLabel(t *testing.T) {
 		JobName: "test",
 		MetricRelabelConfigs: []*relabel.Config{
 			{
-				SourceLabels: model.LabelNames{"le", "__name__"},
-				Regex:        relabel.MustNewRegexp("(\\d+)\\.0+;.*_bucket"),
-				Replacement:  relabel.DefaultRelabelConfig.Replacement,
-				Separator:    relabel.DefaultRelabelConfig.Separator,
-				TargetLabel:  "le",
-				Action:       relabel.Replace,
+				SourceLabels:         model.LabelNames{"le", "__name__"},
+				Regex:                relabel.MustNewRegexp("(\\d+)\\.0+;.*_bucket"),
+				Replacement:          relabel.DefaultRelabelConfig.Replacement,
+				Separator:            relabel.DefaultRelabelConfig.Separator,
+				TargetLabel:          "le",
+				Action:               relabel.Replace,
+				NameValidationScheme: model.UTF8Validation,
 			},
 			{
-				SourceLabels: model.LabelNames{"quantile"},
-				Regex:        relabel.MustNewRegexp("(\\d+)\\.0+"),
-				Replacement:  relabel.DefaultRelabelConfig.Replacement,
-				Separator:    relabel.DefaultRelabelConfig.Separator,
-				TargetLabel:  "quantile",
-				Action:       relabel.Replace,
+				SourceLabels:         model.LabelNames{"quantile"},
+				Regex:                relabel.MustNewRegexp("(\\d+)\\.0+"),
+				Replacement:          relabel.DefaultRelabelConfig.Replacement,
+				Separator:            relabel.DefaultRelabelConfig.Separator,
+				TargetLabel:          "quantile",
+				Action:               relabel.Replace,
+				NameValidationScheme: model.UTF8Validation,
 			},
 		},
 		SampleLimit:                100,
@@ -4862,18 +4868,20 @@ func TestTypeUnitReLabel(t *testing.T) {
 		JobName: "test",
 		MetricRelabelConfigs: []*relabel.Config{
 			{
-				SourceLabels: model.LabelNames{"__name__"},
-				Regex:        relabel.MustNewRegexp(".*_total$"),
-				Replacement:  "counter",
-				TargetLabel:  "__type__",
-				Action:       relabel.Replace,
+				SourceLabels:         model.LabelNames{"__name__"},
+				Regex:                relabel.MustNewRegexp(".*_total$"),
+				Replacement:          "counter",
+				TargetLabel:          "__type__",
+				Action:               relabel.Replace,
+				NameValidationScheme: model.UTF8Validation,
 			},
 			{
-				SourceLabels: model.LabelNames{"__name__"},
-				Regex:        relabel.MustNewRegexp(".*_bytes$"),
-				Replacement:  "bytes",
-				TargetLabel:  "__unit__",
-				Action:       relabel.Replace,
+				SourceLabels:         model.LabelNames{"__name__"},
+				Regex:                relabel.MustNewRegexp(".*_bytes$"),
+				Replacement:          "bytes",
+				TargetLabel:          "__unit__",
+				Action:               relabel.Replace,
+				NameValidationScheme: model.UTF8Validation,
 			},
 		},
 		SampleLimit:                100,
@@ -5482,25 +5490,28 @@ func TestTargetScrapeConfigWithLabels(t *testing.T) {
 				Params:                     url.Values{"param": []string{secondParam}},
 				RelabelConfigs: []*relabel.Config{
 					{
-						Action:       relabel.DefaultRelabelConfig.Action,
-						Regex:        relabel.DefaultRelabelConfig.Regex,
-						SourceLabels: relabel.DefaultRelabelConfig.SourceLabels,
-						TargetLabel:  model.ScrapeTimeoutLabel,
-						Replacement:  expectedTimeoutLabel,
+						Action:               relabel.DefaultRelabelConfig.Action,
+						Regex:                relabel.DefaultRelabelConfig.Regex,
+						SourceLabels:         relabel.DefaultRelabelConfig.SourceLabels,
+						TargetLabel:          model.ScrapeTimeoutLabel,
+						Replacement:          expectedTimeoutLabel,
+						NameValidationScheme: model.UTF8Validation,
 					},
 					{
-						Action:       relabel.DefaultRelabelConfig.Action,
-						Regex:        relabel.DefaultRelabelConfig.Regex,
-						SourceLabels: relabel.DefaultRelabelConfig.SourceLabels,
-						TargetLabel:  paramLabel,
-						Replacement:  expectedParam,
+						Action:               relabel.DefaultRelabelConfig.Action,
+						Regex:                relabel.DefaultRelabelConfig.Regex,
+						SourceLabels:         relabel.DefaultRelabelConfig.SourceLabels,
+						TargetLabel:          paramLabel,
+						Replacement:          expectedParam,
+						NameValidationScheme: model.UTF8Validation,
 					},
 					{
-						Action:       relabel.DefaultRelabelConfig.Action,
-						Regex:        relabel.DefaultRelabelConfig.Regex,
-						SourceLabels: relabel.DefaultRelabelConfig.SourceLabels,
-						TargetLabel:  model.MetricsPathLabel,
-						Replacement:  expectedPath,
+						Action:               relabel.DefaultRelabelConfig.Action,
+						Regex:                relabel.DefaultRelabelConfig.Regex,
+						SourceLabels:         relabel.DefaultRelabelConfig.SourceLabels,
+						TargetLabel:          model.MetricsPathLabel,
+						Replacement:          expectedPath,
+						NameValidationScheme: model.UTF8Validation,
 					},
 				},
 			},

storage/remote/write_test.go
@@ -282,7 +282,8 @@ func TestWriteStorageApplyConfig_PartialUpdate(t *testing.T) {
 		QueueConfig:   config.DefaultQueueConfig,
 		WriteRelabelConfigs: []*relabel.Config{
 			{
-				Regex: relabel.MustNewRegexp(".+"),
+				Regex:                relabel.MustNewRegexp(".+"),
+				NameValidationScheme: model.UTF8Validation,
 			},
 		},
 		ProtobufMessage: config.RemoteWriteProtoMsgV1,
@@ -329,7 +330,10 @@ func TestWriteStorageApplyConfig_PartialUpdate(t *testing.T) {
 
 	storeHashes()
 	// Update c0 and c2.
-	c0.WriteRelabelConfigs[0] = &relabel.Config{Regex: relabel.MustNewRegexp("foo")}
+	c0.WriteRelabelConfigs[0] = &relabel.Config{
+		Regex:                relabel.MustNewRegexp("foo"),
+		NameValidationScheme: model.UTF8Validation,
+	}
 	c2.RemoteTimeout = model.Duration(50 * time.Second)
 	conf = &config.Config{
 		GlobalConfig:       config.GlobalConfig{},