f53bf26cbc2cecd74a1fa6cdfc8142062e82c111
Casie Chen <casie.chen@grafana.com>
2025-08-21 12:29:29 -0700

    Add comments to describe behavior of index.Statistics functions

--- tsdb/index/postings_sketches.go
+++ tsdb/index/postings_sketches.go
@@ -8,8 +8,17 @@ import (
 )
 
 type Statistics interface {
+	// TotalSeries returns the number of series in the TSDB component.
 	TotalSeries() uint64
+
+	// LabelValuesCount returns the number of values for a label name. If the given label name does not exist,
+	// it is valid to return 0 without an error.
 	LabelValuesCount(ctx context.Context, name string) (uint64, error)
+
+	// LabelValuesCardinality returns the cardinality of a given label name (i.e., the number of series which
+	// contain that label name). If values are provided, it returns the combined cardinality of all given values;
+	// otherwise, it returns the total cardinality across all values for the label name. If the label name does not exist,
+	// it is valid to return 0 without an error.
 	LabelValuesCardinality(ctx context.Context, name string, values ...string) (uint64, error)
 }
 
