eaa29e34095d68827a13031f7c762a9b34a6e2ec
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2026-01-16 13:18:49 +0100

    head_chunks_test: use 0 ST for unsupported chunk
    The underlying chunk format is XORchunk, so it doesn't have ST
    anyway.
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- tsdb/chunks/head_chunks_test.go
+++ tsdb/chunks/head_chunks_test.go
@@ -608,7 +608,7 @@ func randomUnsupportedChunk(t *testing.T) chunkenc.Chunk {
 	app, err := chunk.Appender()
 	require.NoError(t, err)
 	for range length {
-		app.Append(rand.Int63(), rand.Float64())
+		app.Append(0, rand.Int63(), rand.Float64())
 	}
 	return chunk
 }
