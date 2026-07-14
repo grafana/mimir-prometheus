prometheus/prometheus

tsdb [PERF]: rebuild shard buckets outside the bucket write lock
Removing deleted series from a shard bucket previously held the bucket
write lock for the whole survivor rebuild: an O(bucket) scan with a
deleted-set probe per ref. Readers that take several bucket locks in
ascending order queue behind the bucket-by-bucket removal march, since a
waiting writer blocks new readers, so low shard counts saw multi-
millisecond read stalls during head garbage collection.

Rebuild survivors from a bucket snapshot without holding the lock, and
only carry over refs appended during the rebuild under the write lock.
This is sound because refs within a snapshot's length are never mutated
in place, and refs appended after the snapshot cannot be among the
deleted refs: a deleted series was added at creation, before its
deletion and this removal, and refs are never reused. A per-bucket
generation, bumped on every refs replacement (re-sorts and removals) but
not on appends, detects the rare concurrent replacement, in which case
removal falls back to rebuilding under the lock as before. Buckets that
contain no deleted refs are now skipped without taking the write lock at
all.

On the concurrent remove/read diagnostic benchmark (2M series), the
bucket snapshot acquisition p99 during removals drops by ~93% overall,
collapsing from milliseconds to single-digit microseconds at
shardCount=16. Read p99 during removals drops 20-62% across dense
shardCount >= 64 (significant in eight of nine cells) and up to 23% in
the sparse cells, with no significant regression in any read p99 cell.
At dense shardCount=16 each read's own eight-bucket merge dominates and
read p99 shows no significant change; read p99 relative to the same
run's no-removal baseline is unchanged in every cell. Pure removal
throughput is unchanged or slightly improved (-11% at the heaviest
churn), with unchanged allocations. Survivor slices reserve 16 refs of
append headroom, which shows as capbytes/live 8.16 vs 8.00 in the
foot

tsdb/shard_bucket_postings_test.go
@@ -270,6 +270,67 @@ func TestShardBucketPostings(t *testing.T) {
 		}
 	})
 
+	t.Run("remove skips buckets without deleted refs", func(t *testing.T) {
+		t.Parallel()
+		s := newShardBucketPostings(2)
+		for ref := storage.SeriesRef(1); ref <= 8; ref++ {
+			s.add(chunks.HeadSeriesRef(ref), uint64(ref))
+		}
+
+		genBefore := s.buckets[0].gen
+		// The deleted refs map to bucket 0 by shard hash but are not present
+		// in it: removal must leave the bucket untouched, without taking its
+		// write lock (observable through the unchanged generation).
+		s.remove(map[storage.SeriesRef]uint64{1000: 0, 1002: 2})
+		require.Equal(t, genBefore, s.buckets[0].gen)
+		require.Equal(t, []storage.SeriesRef{2, 4, 6, 8}, expandShard(t, s, 0, 2))
+	})
+
+	t.Run("remove carries over refs appended during the unlocked rebuild", func(t *testing.T) {
+		t.Parallel()
+		s := newShardBucketPostings(1)
+		for ref := storage.SeriesRef(1); ref <= 6; ref++ {
+			s.add(chunks.HeadSeriesRef(ref), 0)
+		}
+
+		hookRuns := 0
+		s.removeUnlockedHook = func() {
+			// Appends landing between the survivor rebuild and the bucket
+			// re-lock must be carried over, and can never be deleted refs.
+			s.add(7, 0)
+			s.add(8, 0)
+			hookRuns++
+		}
+		s.remove(map[storage.SeriesRef]uint64{2: 0, 4: 0})
+		s.removeUnlockedHook = nil
+
+		require.Equal(t, 1, hookRuns)
+		require.Equal(t, []storage.SeriesRef{1, 3, 5, 6, 7, 8}, expandShard(t, s, 0, 1))
+	})
+
+	t.Run("remove falls back when the bucket is replaced during the unlocked rebuild", func(t *testing.T) {
+		t.Parallel()
+		s := newShardBucketPostings(1)
+		// Out-of-order adds leave the bucket dirty, so a read during the
+		// unlocked rebuild re-sorts it and replaces the refs slice.
+		for _, ref := range []chunks.HeadSeriesRef{2, 6, 4, 8} {
+			s.add(ref, 0)
+		}
+		require.NotEqual(t, cleanShardBucket, s.buckets[0].dirty)
+
+		genBefore := s.buckets[0].gen
+		s.removeUnlockedHook = func() {
+			// Sorts the dirty bucket, bumping the generation.
+			expandShard(t, s, 0, 1)
+		}
+		s.remove(map[storage.SeriesRef]uint64{4: 0})
+		s.removeUnlockedHook = nil
+
+		// One bump from the re-sort, one from the fallback rebuild.
+		require.Equal(t, genBefore+2, s.buckets[0].gen)
+		require.Equal(t, []storage.SeriesRef{2, 6, 8}, expandShard(t, s, 0, 1))
+	})
+
 	t.Run("out-of-order adds are served sorted", func(t *testing.T) {
 		t.Parallel()
 		s := newShardBucketPostings(1)

grafana/mimir-prometheus

tsdb/shard_bucket_postings_test.go
@@ -270,6 +270,67 @@ func TestShardBucketPostings(t *testing.T) {
 		}
 	})
 
+	t.Run("remove skips buckets without deleted refs", func(t *testing.T) {
+		t.Parallel()
+		s := newShardBucketPostings(2)
+		for ref := storage.SeriesRef(1); ref <= 8; ref++ {
+			s.add(chunks.HeadSeriesRef(ref), uint64(ref))
+		}
+
+		genBefore := s.buckets[0].gen
+		// The deleted refs map to bucket 0 by shard hash but are not present
+		// in it: removal must leave the bucket untouched, without taking its
+		// write lock (observable through the unchanged generation).
+		s.remove(map[storage.SeriesRef]uint64{1000: 0, 1002: 2})
+		require.Equal(t, genBefore, s.buckets[0].gen)
+		require.Equal(t, []storage.SeriesRef{2, 4, 6, 8}, expandShardCandidates(t, s, 0, 2))
+	})
+
+	t.Run("remove carries over refs appended during the unlocked rebuild", func(t *testing.T) {
+		t.Parallel()
+		s := newShardBucketPostings(1)
+		for ref := storage.SeriesRef(1); ref <= 6; ref++ {
+			s.add(chunks.HeadSeriesRef(ref), 0)
+		}
+
+		hookRuns := 0
+		s.removeUnlockedHook = func() {
+			// Appends landing between the survivor rebuild and the bucket
+			// re-lock must be carried over, and can never be deleted refs.
+			s.add(7, 0)
+			s.add(8, 0)
+			hookRuns++
+		}
+		s.remove(map[storage.SeriesRef]uint64{2: 0, 4: 0})
+		s.removeUnlockedHook = nil
+
+		require.Equal(t, 1, hookRuns)
+		require.Equal(t, []storage.SeriesRef{1, 3, 5, 6, 7, 8}, expandShardCandidates(t, s, 0, 1))
+	})
+
+	t.Run("remove falls back when the bucket is replaced during the unlocked rebuild", func(t *testing.T) {
+		t.Parallel()
+		s := newShardBucketPostings(1)
+		// Out-of-order adds leave the bucket dirty, so a read during the
+		// unlocked rebuild re-sorts it and replaces the refs slice.
+		for _, ref := range []chunks.HeadSeriesRef{2, 6, 4, 8} {
+			s.add(ref, 0)
+		}
+		require.NotEqual(t, cleanShardBucket, s.buckets[0].dirty)
+
+		genBefore := s.buckets[0].gen
+		s.removeUnlockedHook = func() {
+			// Sorts the dirty bucket, bumping the generation.
+			expandShardCandidates(t, s, 0, 1)
+		}
+		s.remove(map[storage.SeriesRef]uint64{4: 0})
+		s.removeUnlockedHook = nil
+
+		// One bump from the re-sort, one from the fallback rebuild.
+		require.Equal(t, genBefore+2, s.buckets[0].gen)
+		require.Equal(t, []storage.SeriesRef{2, 6, 8}, expandShardCandidates(t, s, 0, 1))
+	})
+
 	t.Run("out-of-order adds are served sorted", func(t *testing.T) {
 		t.Parallel()
 		s := newShardBucketPostings(1)
@@ -631,6 +692,7 @@ var shardBucketSparseBenchmarkBuckets = []uint64{0, 32, 64, 96}
 func shardBucketDeletePatterns() []shardBucketDeletePattern {
 	return []shardBucketDeletePattern{
 		{name: "dense/churn=1/2", every: 2},
+		{name: "dense/churn=1/3", every: 3},
 		{name: "dense/churn=1/8", every: 8},
 		{name: "sparse/spread4/churn=1/8", every: 8, sparseBuckets: shardBucketSparseBenchmarkBuckets},
 	}