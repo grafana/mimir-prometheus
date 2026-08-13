prometheus/prometheus

tsdb: Skip clean series during periodic head chunk mmap (#18272)
tsdb: Skip clean series during periodic head chunk mmap

The periodic mmapHeadChunks cycle previously acquired a per-series
lock on every series, even though typically >99% have nothing to
mmap. This was identified as a CPU bottleneck in Grafana Mimir.

Add a headChunkCount field (sync/atomic.Uint32) to memSeries that
tracks the number of head chunks. It is incremented in
cutNewHeadChunk and the histogram new-chunk paths, and reset by
mmapChunks and truncateChunksBefore. mmapHeadChunks uses a lock-free
Load to skip series with fewer than 2 head chunks, avoiding the
per-series lock for clean series.

sync/atomic.Uint32 (4 bytes) is used instead of go.uber.org/atomic
(8 bytes) to fit in existing struct padding without growing
memSeries. Chunk counts are bounded by the 3-byte field in
HeadChunkRef, so cannot overflow uint32.

Also fix pre-existing comment inaccuracies in the touched code:
headChunks.next -> headChunks.prev, mmapHeadChunks() -> mmapChunks()
in the doc comment, and a grammar error.

---------

Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

tsdb/head_bench_test.go
@@ -311,3 +311,75 @@ type failingSeriesLifecycleCallback struct{}
 func (failingSeriesLifecycleCallback) PreCreation(labels.Labels) error                     { return errors.New("failed") }
 func (failingSeriesLifecycleCallback) PostCreation(labels.Labels)                          {}
 func (failingSeriesLifecycleCallback) PostDeletion(map[chunks.HeadSeriesRef]labels.Labels) {}
+
+func BenchmarkMmapHeadChunks(b *testing.B) {
+	for _, seriesCount := range []int{1000, 10000, 100000} {
+		for _, readyPct := range []float64{0.01, 0.1, 1.0} {
+			readyCount := max(int(float64(seriesCount)*readyPct), 1)
+			b.Run(fmt.Sprintf("series=%d/ready=%d", seriesCount, readyCount), func(b *testing.B) {
+				db := newTestDB(b)
+				db.DisableCompactions()
+				chunkRange := DefaultBlockDuration
+
+				// Create all series in batches.
+				refs := make([]storage.SeriesRef, seriesCount)
+				lblsPerSeries := make([]labels.Labels, seriesCount)
+				ts := int64(0)
+				const batchSize = 1000
+				for batchStart := 0; batchStart < seriesCount; batchStart += batchSize {
+					app := db.Appender(b.Context())
+					batchEnd := min(batchStart+batchSize, seriesCount)
+					for i := batchStart; i < batchEnd; i++ {
+						lbls := labels.FromStrings("__name__", "bench", "i", strconv.Itoa(i))
+						lblsPerSeries[i] = lbls
+						ref, err := app.Append(0, lbls, ts, float64(i))
+						require.NoError(b, err)
+						refs[i] = ref
+					}
+					require.NoError(b, app.Commit())
+				}
+
+				// Pick a random, spread-out set of series to make ready each iteration.
+				// Using a Fisher-Yates shuffle prefix ensures coverage across stripes.
+				rng := rand.New(rand.NewSource(42))
+				perm := rng.Perm(seriesCount)
+				readyIndices := perm[:readyCount]
+
+				// makeReady appends a sample past the chunk range boundary to
+				// trigger a new head chunk on each ready series. Two appends
+				// suffice: one near the current ts, one past nextAt.
+				makeReady := func() {
+					ts++ // small increment for first sample
+					app := db.Appender(b.Context())
+					for _, idx := range readyIndices {
+						var err error
+						refs[idx], err = app.Append(refs[idx], lblsPerSeries[idx], ts, float64(ts))
+						require.NoError(b, err)
+					}
+					require.NoError(b, app.Commit())
+
+					ts += chunkRange // jump past nextAt to force chunk cut
+					app = db.Appender(b.Context())
+					for _, idx := range readyIndices {
+						var err error
+						refs[idx], err = app.Append(refs[idx], lblsPerSeries[idx], ts, float64(ts))
+						require.NoError(b, err)
+					}
+					require.NoError(b, app.Commit())
+				}
+
+				makeReady()
+
+				b.ResetTimer()
+				b.ReportAllocs()
+
+				for range b.N {
+					db.ForceHeadMMap()
+					b.StopTimer()
+					makeReady()
+					b.StartTimer()
+				}
+			})
+		}
+	}
+}

grafana/mimir-prometheus

Backport adaptations for mimir-release-2.17:
- Use context.Background() and openTestDB with explicit cleanup because
  this branch uses Go 1.23 and predates the upstream helper cleanup.
- Preserve the #19136 cache behavior by using headChunkCount to
  pre-size head-chunk cache collections.
- Normalize the counter in the release branch truncation fixture and
  close the added test heads without asserting while series locks are held.
(cherry picked from commit 98809e40c64bd25d3dc4b01c1d4780713e1b3347)

tsdb/head_bench_test.go
@@ -171,3 +171,78 @@ type failingSeriesLifecycleCallback struct{}
 func (failingSeriesLifecycleCallback) PreCreation(labels.Labels) error                     { return errors.New("failed") }
 func (failingSeriesLifecycleCallback) PostCreation(labels.Labels)                          {}
 func (failingSeriesLifecycleCallback) PostDeletion(map[chunks.HeadSeriesRef]labels.Labels) {}
+
+func BenchmarkMmapHeadChunks(b *testing.B) {
+	for _, seriesCount := range []int{1000, 10000, 100000} {
+		for _, readyPct := range []float64{0.01, 0.1, 1.0} {
+			readyCount := max(int(float64(seriesCount)*readyPct), 1)
+			b.Run(fmt.Sprintf("series=%d/ready=%d", seriesCount, readyCount), func(b *testing.B) {
+				db := openTestDB(b, nil, nil)
+				b.Cleanup(func() {
+					require.NoError(b, db.Close())
+				})
+				db.DisableCompactions()
+				chunkRange := DefaultBlockDuration
+
+				// Create all series in batches.
+				refs := make([]storage.SeriesRef, seriesCount)
+				lblsPerSeries := make([]labels.Labels, seriesCount)
+				ts := int64(0)
+				const batchSize = 1000
+				for batchStart := 0; batchStart < seriesCount; batchStart += batchSize {
+					app := db.Appender(context.Background())
+					batchEnd := min(batchStart+batchSize, seriesCount)
+					for i := batchStart; i < batchEnd; i++ {
+						lbls := labels.FromStrings("__name__", "bench", "i", strconv.Itoa(i))
+						lblsPerSeries[i] = lbls
+						ref, err := app.Append(0, lbls, ts, float64(i))
+						require.NoError(b, err)
+						refs[i] = ref
+					}
+					require.NoError(b, app.Commit())
+				}
+
+				// Pick a random, spread-out set of series to make ready each iteration.
+				// Using a Fisher-Yates shuffle prefix ensures coverage across stripes.
+				rng := rand.New(rand.NewSource(42))
+				perm := rng.Perm(seriesCount)
+				readyIndices := perm[:readyCount]
+
+				// makeReady appends a sample past the chunk range boundary to
+				// trigger a new head chunk on each ready series. Two appends
+				// suffice: one near the current ts, one past nextAt.
+				makeReady := func() {
+					ts++ // small increment for first sample
+					app := db.Appender(context.Background())
+					for _, idx := range readyIndices {
+						var err error
+						refs[idx], err = app.Append(refs[idx], lblsPerSeries[idx], ts, float64(ts))
+						require.NoError(b, err)
+					}
+					require.NoError(b, app.Commit())
+
+					ts += chunkRange // jump past nextAt to force chunk cut
+					app = db.Appender(context.Background())
+					for _, idx := range readyIndices {
+						var err error
+						refs[idx], err = app.Append(refs[idx], lblsPerSeries[idx], ts, float64(ts))
+						require.NoError(b, err)
+					}
+					require.NoError(b, app.Commit())
+				}
+
+				makeReady()
+
+				b.ResetTimer()
+				b.ReportAllocs()
+
+				for range b.N {
+					db.ForceHeadMMap()
+					b.StopTimer()
+					makeReady()
+					b.StartTimer()
+				}
+			})
+		}
+	}
+}