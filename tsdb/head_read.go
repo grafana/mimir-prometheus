prometheus/prometheus

tsdb: harden head-chunk cache lifecycle (#19136)
Collapse the cache fingerprint into a comparable headChunkCacheKey so
the hit check, populate, and reset are single expressions and future
fingerprint fields cannot be forgotten in the comparison.

Bound the cache buffer: apply the headChunksBufMaxCap policy when
moving to a different series, pre-size replacement collections from
headChunkCount, and release the cache on Close. Route the OOO wrapper's
cache enablement and Close through the headChunkReader methods and
release its fallback collection buffer on Close, and propagate chunk
reader Close errors in the head-and-OOO queriers.

Zero the collection tail inside collectHeadChunks itself instead of at
every call site, and document the contract there.

Test the cache-hit path with a sentinel (a re-collection for the same
series rewrites the same backing array, so pointer identity of the
slice cannot distinguish a hit from a miss), extract a shared test-head
helper, and assert outside the series lock so a failing assertion
cannot deadlock the cleanup's Close.

Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

tsdb/head_read.go
@@ -386,7 +386,6 @@ func appendSeriesChunks(s *memSeries, mint, maxt int64, chks []chunks.Meta, head
 
 	// Multiple head chunks: collect once O(N), iterate O(N).
 	headChunksBuf = collectHeadChunks(s.headChunks, headChunksBuf[:0])
-	clear(headChunksBuf[len(headChunksBuf):cap(headChunksBuf)])
 	for i, chk := range headChunksBuf {
 		maxTime := chk.maxTime
 		if i == len(headChunksBuf)-1 {
@@ -491,18 +490,47 @@ type headChunkReader struct {
 	enableCache bool
 	// Cache for head chunks — avoids O(n²) linked-list walks when
 	// iterating all chunks of a series oldest-to-newest.
-	cachedSeriesRef      storage.SeriesRef
-	cachedHeadChunks     []*memChunk
-	cachedHeadChunksHead *memChunk          // Head pointer at collection time; detects head-chunk replacement.
-	cachedMmapLen        int                // len(s.mmappedChunks) at collection time; detects mmap events.
-	cachedFirstChunkID   chunks.HeadChunkID // s.firstChunkID at collection time; detects truncation.
+	cachedKey        headChunkCacheKey
+	cachedHeadChunks []*memChunk
+}
+
+// headChunkCacheKey identifies the chunk-layout state of a series at
+// collection time; any layout change (append, mmapping, truncation) alters at
+// least one field, invalidating the cached slice.
+type headChunkCacheKey struct {
+	ref          storage.SeriesRef
+	head         *memChunk          // Head pointer; detects head-chunk replacement.
+	mmapLen      int                // len(s.mmappedChunks); detects mmap events.
+	firstChunkID chunks.HeadChunkID // Detects truncation.
+}
+
+// headChunkCacheKeyFor returns the cache key for the current chunk layout of s.
+// The series lock must be held.
+func headChunkCacheKeyFor(s *memSeries) headChunkCacheKey {
+	return headChunkCacheKey{
+		ref:          storage.SeriesRef(s.ref),
+		head:         s.headChunks,
+		mmapLen:      len(s.mmappedChunks),
+		firstChunkID: s.firstChunkID,
+	}
 }
 
 func (h *headChunkReader) Close() error {
 	if h.isoState != nil {
 		h.isoState.Close()
 	}
+	// Release the cache so a closed reader retains no chunk data.
+	h.cachedKey = headChunkCacheKey{}
+	h.cachedHeadChunks = nil
 	return nil
+}
+
+// EnableChunkCache enables the head-chunk cache for sequential chunk access
+// patterns (range queries), which look up every chunk of a series. The cache
+// is invalidated whenever the series' chunk layout changes (a parallel append,
+// mmapping, or truncation), falling back to O(n) for that lookup.
+func (h *headChunkReader) EnableChunkCache() {
+	h.enableCache = true
 }
 
 // getOrCollectHeadChunks returns the cached head-chunk slice for s, collecting
@@ -515,23 +543,21 @@ func (h *headChunkReader) getOrCollectHeadChunks(s *memSeries) []*memChunk {
 		return nil
 	}
 
-	ref := storage.SeriesRef(s.ref)
-	if ref == h.cachedSeriesRef && s.headChunks == h.cachedHeadChunksHead &&
-		h.cachedMmapLen == len(s.mmappedChunks) && h.cachedFirstChunkID == s.firstChunkID {
+	key := headChunkCacheKeyFor(s)
+	if key == h.cachedKey {
 		return h.cachedHeadChunks
 	}
 
-	var buf []*memChunk
-	if h.cachedHeadChunks != nil {
-		buf = h.cachedHeadChunks[:0]
+	buf := h.cachedHeadChunks[:0]
+	// Pre-size the first collection, and do not carry oversized backing arrays
+	// from one series to the next (the headChunksBufMaxCap policy used for
+	// headChunksBuf) — but keep a large array while it still serves the same
+	// series.
+	if c := cap(buf); c == 0 || (c > headChunksBufMaxCap && key.ref != h.cachedKey.ref) {
+		buf = make([]*memChunk, 0, s.headChunkCount.Load())
 	}
 	h.cachedHeadChunks = collectHeadChunks(s.headChunks, buf)
-	// Allow GC of *memChunk pointers left over from a previous, longer collection.
-	clear(h.cachedHeadChunks[len(h.cachedHeadChunks):cap(h.cachedHeadChunks)])
-	h.cachedSeriesRef = ref
-	h.cachedHeadChunksHead = s.headChunks
-	h.cachedMmapLen = len(s.mmappedChunks)
-	h.cachedFirstChunkID = s.firstChunkID
+	h.cachedKey = key
 	return h.cachedHeadChunks
 }
 

grafana/mimir-prometheus

Backport adaptations for this branch:
- memSeries has no headChunkCount, so replacement collections are
  pre-sized from headChunks.len() instead.
- newTestHeadWithOptions does not register a cleanup here, so the new
  tests close the head themselves.
- t.Context() is Go 1.24, this branch is on Go 1.23, so the tests use
  context.Background().
(cherry picked from commit f92530057d374fe2ca7ee6f614a727e6050ff416)
Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

tsdb/head_read.go
@@ -263,7 +263,6 @@ func appendSeriesChunks(s *memSeries, mint, maxt int64, chks []chunks.Meta, head
 
 	// Multiple head chunks: collect once O(N), iterate O(N).
 	headChunksBuf = collectHeadChunks(s.headChunks, headChunksBuf[:0])
-	clear(headChunksBuf[len(headChunksBuf):cap(headChunksBuf)])
 	for i, chk := range headChunksBuf {
 		maxTime := chk.maxTime
 		if i == len(headChunksBuf)-1 {
@@ -383,18 +382,47 @@ type headChunkReader struct {
 	enableCache bool
 	// Cache for head chunks — avoids O(n²) linked-list walks when
 	// iterating all chunks of a series oldest-to-newest.
-	cachedSeriesRef      storage.SeriesRef
-	cachedHeadChunks     []*memChunk
-	cachedHeadChunksHead *memChunk          // Head pointer at collection time; detects head-chunk replacement.
-	cachedMmapLen        int                // len(s.mmappedChunks) at collection time; detects mmap events.
-	cachedFirstChunkID   chunks.HeadChunkID // s.firstChunkID at collection time; detects truncation.
+	cachedKey        headChunkCacheKey
+	cachedHeadChunks []*memChunk
+}
+
+// headChunkCacheKey identifies the chunk-layout state of a series at
+// collection time; any layout change (append, mmapping, truncation) alters at
+// least one field, invalidating the cached slice.
+type headChunkCacheKey struct {
+	ref          storage.SeriesRef
+	head         *memChunk          // Head pointer; detects head-chunk replacement.
+	mmapLen      int                // len(s.mmappedChunks); detects mmap events.
+	firstChunkID chunks.HeadChunkID // Detects truncation.
+}
+
+// headChunkCacheKeyFor returns the cache key for the current chunk layout of s.
+// The series lock must be held.
+func headChunkCacheKeyFor(s *memSeries) headChunkCacheKey {
+	return headChunkCacheKey{
+		ref:          storage.SeriesRef(s.ref),
+		head:         s.headChunks,
+		mmapLen:      len(s.mmappedChunks),
+		firstChunkID: s.firstChunkID,
+	}
 }
 
 func (h *headChunkReader) Close() error {
 	if h.isoState != nil {
 		h.isoState.Close()
 	}
+	// Release the cache so a closed reader retains no chunk data.
+	h.cachedKey = headChunkCacheKey{}
+	h.cachedHeadChunks = nil
 	return nil
+}
+
+// EnableChunkCache enables the head-chunk cache for sequential chunk access
+// patterns (range queries), which look up every chunk of a series. The cache
+// is invalidated whenever the series' chunk layout changes (a parallel append,
+// mmapping, or truncation), falling back to O(n) for that lookup.
+func (h *headChunkReader) EnableChunkCache() {
+	h.enableCache = true
 }
 
 // getOrCollectHeadChunks returns the cached head-chunk slice for s, collecting
@@ -407,23 +435,21 @@ func (h *headChunkReader) getOrCollectHeadChunks(s *memSeries) []*memChunk {
 		return nil
 	}
 
-	ref := storage.SeriesRef(s.ref)
-	if ref == h.cachedSeriesRef && s.headChunks == h.cachedHeadChunksHead &&
-		h.cachedMmapLen == len(s.mmappedChunks) && h.cachedFirstChunkID == s.firstChunkID {
+	key := headChunkCacheKeyFor(s)
+	if key == h.cachedKey {
 		return h.cachedHeadChunks
 	}
 
-	var buf []*memChunk
-	if h.cachedHeadChunks != nil {
-		buf = h.cachedHeadChunks[:0]
+	buf := h.cachedHeadChunks[:0]
+	// Pre-size the first collection, and do not carry oversized backing arrays
+	// from one series to the next (the headChunksBufMaxCap policy used for
+	// headChunksBuf) — but keep a large array while it still serves the same
+	// series.
+	if c := cap(buf); c == 0 || (c > headChunksBufMaxCap && key.ref != h.cachedKey.ref) {
+		buf = make([]*memChunk, 0, s.headChunks.len())
 	}
 	h.cachedHeadChunks = collectHeadChunks(s.headChunks, buf)
-	// Allow GC of *memChunk pointers left over from a previous, longer collection.
-	clear(h.cachedHeadChunks[len(h.cachedHeadChunks):cap(h.cachedHeadChunks)])
-	h.cachedSeriesRef = ref
-	h.cachedHeadChunksHead = s.headChunks
-	h.cachedMmapLen = len(s.mmappedChunks)
-	h.cachedFirstChunkID = s.firstChunkID
+	h.cachedKey = key
 	return h.cachedHeadChunks
 }
 